199ca83cfc4b392e47113d256e329f13b0799eba
caninus <caninus@caninuss-MacBook-Pro.local>
2024-01-30 00:23:21 +0900

    push

--- web/nuxt/pages/index.vue
+++ web/nuxt/pages/index.vue
@@ -3,4 +3,17 @@
 </template>
 <script setup lang="ts">
 
+console.log('index!!')
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { collection, doc, getDoc } from "firebase/firestore"; 
+// import * as firebase_config from '../../../firebase-config.js'
+
+// const app = initializeApp(firebase_config);
+// const db = getFirestore(app)
+// const citiesRef = collection(db, "users");
+// const a = await getDoc(citiesRef)
+// console.log('citiesRef',citiesRef)
 </script>
+
+
