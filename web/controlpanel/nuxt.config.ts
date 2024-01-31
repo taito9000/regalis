4deed896c330d59db2a12415b7def42e47e3d030
caninus <caninus@caninuss-MacBook-Pro.local>
2024-01-31 22:43:58 +0900

    remake nuxt as controlpanel

--- web/controlpanel/nuxt.config.ts
+++ web/controlpanel/nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  nitro: {
-    firebase: {
-      gen: 2
-    }
-  }
+  devtools: { enabled: true }
 })
